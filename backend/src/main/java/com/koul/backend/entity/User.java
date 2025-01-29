63e40f3da37ee375090169c5def98eaa75459ece
Jang Dong Ik <tomist0930@gmail.com>
2025-01-29 21:36:11 +0900

    config: jpa 세팅

---
+++ backend/src/main/java/com/koul/backend/entity/User.java
@@ -0,0 +1,38 @@
+package com.koul.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    private LocalDateTime lastLogin;
+
+    @CreatedDate
+    @Column(insertable = true)
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    private LocalDateTime updatedDate;
+
+}
